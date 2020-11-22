7820a42f906afbae40c75072cd3e213a6fa91d36
n-kremeris <n.kremeris@live.com>
2020-11-22 19:55:24 +0000

    Ver 0.1

--- focus_peaking.py
+++ focus_peaking.py
@@ -3,33 +3,62 @@ import numpy as np
 import numexpr as ne
 
 def main():
-    cap = cv2.VideoCapture(2)
+
+    #Use Video4linux backend 
+    #(default is gstreamer which does not support fourcc settings)
+    cap = cv2.VideoCapture(2, cv2.CAP_V4L)
+    
+    #set four character codec to MJPEG 
+    #for cheap (~10gbp hdmi capture cards) default is YUYV at max. 5 FPS
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
+    #set device capture resolution
     cap.set(3,1920)
     cap.set(4,1080)
+    
+    #kernel for edge dilation (make thicker)
+    kernel = np.ones((5,5),np.uint8)
 
-    frame_pre, src_pre = cap.read()
-     
-    scale_percent = 20 # percent of original size
-    scaled_width = int(src_pre.shape[1] * scale_percent / 100)
-    scaled_height = int(src_pre.shape[0] * scale_percent / 100)
-    scaled_dim = (scaled_width, scaled_height) 
-    orig_dim = (src_pre.shape[1], src_pre.shape[0])
-
-    while(True):
-        frame, src_raw = cap.read()
-        src = src_raw
-        #src = cv2.resize(src_raw, scaled_dim, interpolation = cv2.INTER_AREA)
-        #gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-        #edges = cv2.Canny(gray,150,250)
+    while(True):   
+        #read frame from device
+        frame, src = cap.read()
         
-        #edges = cv2.resize(edges, orig_dim, cv2.INTER_AREA)
-        #edges_bgr = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-        #edges_bgr[edges_bgr==0] = 1
-        #img = cv2.multiply(edges_bgr, src_raw)
-        
-        cv2.imshow("end", src_raw)
-        #cv2.imshow("gray", img)
+        #==== Focus peak detection ====#
+        #convert frame to grayscale
+        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
+        #equalize histogram
+        gray = cv2.equalizeHist(gray) 
+
+        #blur grayscale image
+        blur = cv2.GaussianBlur(gray,(7,7),0)
+        
+        #run edge detection
+        edges = cv2.Canny(gray,50,250)
+
+        #dilate detected edges to make them bigger
+        edges = cv2.dilate(edges,kernel,iterations = 1)
+        
+        #==== Edge overlay on original image ====#
+        #Create mask from detected edges
+        ret, mask = cv2.threshold(edges, 10, 255, cv2.THRESH_BINARY)
+
+        #cut out the edges from their background
+        fg = cv2.bitwise_or(edges, edges, mask=mask)
+
+        #convert edges foreground to color
+        fg = cv2.cvtColor(fg, cv2.COLOR_GRAY2BGR)
+
+        #invert edge mask
+        mask_inv = cv2.bitwise_not(mask)
+
+        #remove edges foreground from original image
+        bg = cv2.bitwise_or(src_raw, src_raw, mask=mask_inv)
+
+        #combine foreground edges with background image
+        result = cv2.bitwise_or(fg,bg)
+        
+        cv2.imshow("end", result)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
