91d0b4dbe9e001c57d49fe3acc34de736d956dc3
n-kremeris <n.kremeris@live.com>
2020-11-23 23:16:25 +0000

    separate out focus peaking + overlay
    move focus peaking out to separate class
    move fg/bg overlay out to separate function

--- focus_peaking.py
+++ focus_peaking.py
@@ -7,6 +7,7 @@ cfg_focuspeaking_enable = True
 # Grid configuration
 cfg_grid_enable = True; #enable drawing of grid
 cfg_grid_div = 3 # grid divisions
+cfg_grid_thiccness = 3 # grid line width
 
 # Resolution of video capture
 cfg_capture_width = 1600
@@ -17,19 +18,21 @@ cfg_draw_width = 1600
 cfg_draw_height = 900
 
 # Misc config
-cfg_mirror = True # Mirror the image
-cfg_fullscreen_enable = True # Fullscreen display of result
+cfg_mirror_enable = True # Mirror the image
+cfg_fullscreen_enable = True # Fullscreen display of image
 
 
+#==== Class to draw a grid on the output frame ====#
 class Grid:
-    lines = [] 
+    lines = []
     x_div = 0
     y_div = 0
-    
+
     # Precalculate required lines in advance, requires target dimensions
-    def __init__(self, target_dims, divisor):
+    def __init__(self, target_dims, divisor, thickness):
         self.target_dims = target_dims
         self.divisor = divisor
+        self.thickness = thickness
 
         # x and y division length is width and heigth divided by the grid divisor
         # truncate to integer value
@@ -56,112 +59,124 @@ class Grid:
 
     def Draw(self, image):
         for line in self.lines:
-            image = cv2.line(image, line[0], line[1], (0, 0, 255), 1, 1)
+            image = cv2.line(image, line[0], line[1], (0, 0, 255), self.thickness, 1)
 
         return image
 
 
 #==== Focus peak detection ====#
-
 class FocusPeakDetector:
-    def __init__(self, th1, th2, blur_size, dilate_size, dilate_iter)
+    def __init__(self, th1, th2, blur_size, dilate_size, dilate_iter):
+        self.th1 = th1
+        self.th2 = th2
+        self.blur_kernel = (blur_size, blur_size)
+        self.dilate_kernel = np.ones((dilate_size, dilate_size),np.uint8) #kernel for dilate size (makes lines thicker)
+        self.dilate_iter = dilate_iter #number of iterations to run dilation
 
-def detect_focus_edges
-    #convert frame to grayscale
-    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
- 
-    #equalize histogram
-    gray = cv2.equalizeHist(gray) 
- 
-    #blur grayscale image
-    blur = cv2.GaussianBlur(gray,(3,3),0)
-    
-    #run edge detection
-    edges = cv2.Canny(gray,200,300)
- 
-    #dilate detected edges to make them bigger
-    edges = cv2.dilate(edges,kernel,iterations = 1)
+    def Detect(self, image):
+        #convert frame to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    return edges;
+        #equalize histogram (contrast stretch)
+        gray = cv2.equalizeHist(gray)
+
+        #blur grayscale image
+        blur = cv2.GaussianBlur(gray,self.blur_kernel,0)
+
+        #run edge detection
+        edges = cv2.Canny(gray, self.th1, self.th2)
+
+        #dilate detected edges to make them bigger
+        edges = cv2.dilate(edges, self.dilate_kernel, iterations = self.dilate_iter)
+
+        #create binary mask of detected edges
+        ret, mask = cv2.threshold(edges, 10, 255, cv2.THRESH_BINARY)
+
+        return (mask, edges)
+
+
+#==== Overlay fb over bg with given mask ====#
+def overlay(fg, bg, mask):
+
+    #cut out the edges from their background
+    fg = cv2.bitwise_or(fg, fg, mask=mask)
+
+    #convert edges foreground to color
+    fg = cv2.cvtColor(fg, cv2.COLOR_GRAY2BGR)
+
+    #invert edge mask
+    mask_inv = cv2.bitwise_not(mask)
+
+    #remove foreground from background
+    bg = cv2.bitwise_or(bg, bg, mask=mask_inv)
+
+    #combine foreground edges with background image
+    return cv2.bitwise_or(fg,bg)
 
 
 def main():
+    global cfg_focuspeaking_enable
+    global cfg_mirror_enable
 
-    #Use Video4linux backend 
+    #Use Video4linux backend
     #(default is gstreamer which does not support fourcc settings)
     cap = cv2.VideoCapture(0, cv2.CAP_V4L)
-    
-    #set four character codec to MJPEG 
+
+    #set four character codec to MJPEG
     #for cheap (~10gbp hdmi capture cards) default is YUYV at max. 5 FPS
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
     #set device capture resolution
-
     cap.set(3,cfg_capture_width)
     cap.set(4,cfg_capture_height)
-    
 
-    #kernel for edge dilation (make thicker)
-    kernel = np.ones((5,5),np.uint8)
-    
     # resizing detection
     capture_dims = (cfg_capture_width, cfg_capture_height)
     target_dims = (cfg_draw_width, cfg_draw_height)
-    resize_f = True
+    resize_f = (capture_dims == target_dims)
 
-    current_grid = Grid(target_dims, cfg_grid_div)
+    grid = Grid(target_dims, cfg_grid_div, cfg_grid_thiccness)
+    fpdetector = FocusPeakDetector(200, 300, 3, 3, 1)
 
-    while(True):   
-        #read frame from device
-        frame, src = cap.read()
-        
-                
-        #==== Edge overlay on original image ====#
-        #Create mask from detected edges
-        ret, mask = cv2.threshold(edges, 10, 255, cv2.THRESH_BINARY)
+    while(True):
+        # Read frame from device
+        frame, image = cap.read()
 
-        #cut out the edges from their background
-        fg = cv2.bitwise_or(edges, edges, mask=mask)
+        if (cfg_focuspeaking_enable):
+            # Detect focus peaks
+            mask, peaks = fpdetector.Detect(image)
+            # Overlay focus peaks on original image
+            image = overlay(peaks, image, mask)
 
-        #convert edges foreground to color
-        fg = cv2.cvtColor(fg, cv2.COLOR_GRAY2BGR)
-
-        #invert edge mask
-        mask_inv = cv2.bitwise_not(mask)
-
-        #remove edges foreground from original image
-        bg = cv2.bitwise_or(src, src, mask=mask_inv)
-
-        #combine foreground edges with background image
-        result = cv2.bitwise_or(fg,bg)
-        
-        #resize if target dimensions dont match capture dimensions
+        # Resize if target dimensions dont match capture dimensions
         if (resize_f):
-            result = cv2.resize(result, target_dims)
+            image = cv2.resize(image, target_dims)
 
-        #flip image horisontaly if mirroring enabled
-        if (cfg_mirror):
-            result = cv2.flip(result, 1)
-            
-        # draw grid
+        # Flip image horisontaly if mirroring enabled
+        if (cfg_mirror_enable):
+            image = cv2.flip(image, 1)
+
+        # Draw grid
         if (cfg_grid_enable):
-            result = current_grid.Draw(result)
+            image = grid.Draw(image)
 
-        #set result window properties to fullscreen if enabled
+        # Set image window properties to fullscreen if enabled
         if (cfg_fullscreen_enable):
-            cv2.namedWindow("result", cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty("result",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+            cv2.namedWindow("image", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("image",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
-        # Display result and wait for "q" to quit
-        cv2.imshow("result", result)
+        # Display image and wait for "q" to quit, or other options
+        cv2.imshow("image", image)
         key = cv2.waitKey(1)
 
         if key & 0xFF == ord('q'):
             break
 
         if key & 0xff == ord('f'):
-            global cfg_focuspeaking_enable
             cfg_focuspeaking_enable = not cfg_focuspeaking_enable
+
+        if key & 0xff == ord('m'):
+            cfg_mirror_enable = not cfg_mirror_enable
 
 
 if __name__ == "__main__":
